9370ba2843b32f6d9d850fec24d30578bd6a3507
황범석 <bitofsky@kokomogames.co.kr>
2016-09-29 18:00:36 +0900

    test update

--- test/test.js
+++ test/test.js
@@ -15,7 +15,10 @@ class Human extends ClassHelper {
 
 describe('Test - Singleton check', () => {
 
-    function doCheck() {
+    function doCheck(algorithm) {
+
+        ClassHelper.changeHashAlgorithm(algorithm);
+
         let oJone = Human.getInstance(container, 'Jone');
         let oSteve = Human.getInstance(container, 'Steve');
         let oSteve2 = Human.getInstance(container, 'Steve');
@@ -35,46 +38,29 @@ describe('Test - Singleton check', () => {
         });
     }
 
-    describe('MD5', () => {
-        ClassHelper.changeHashAlgorithm('md5');
-        doCheck();
-    });
-
-    describe('xxHash - 32bit', () => {
-        ClassHelper.changeHashAlgorithm('xxhash32');
-        doCheck();
-    });
-
-    describe('xxHash - 64bit', () => {
-        ClassHelper.changeHashAlgorithm('xxhash64');
-        doCheck();
-    });
+    describe('MD5', () => doCheck('md5'));
+    describe('xxHash - 32bit', doCheck('xxhash32'));
+    describe('xxHash - 64bit', doCheck('xxhash64'));
 
 });
 
 describe('Benchmark - Singleton arguments key generation', () => {
 
-    function doLoop(done) {
+    function doLoop(algorithm, done) {
+
+        ClassHelper.changeHashAlgorithm(algorithm);
+
         for (let i = 0; i < 10000; i++) {
             let oJone = Human.getInstance(container, 'Jone');
             assert.equal(oJone.name, 'Jone');
         }
+
         done();
+
     }
 
-    it('MD5', done => {
-        ClassHelper.changeHashAlgorithm('md5');
-        doLoop(done);
-    });
-
-    it('xxHash - 32bit', done => {
-        ClassHelper.changeHashAlgorithm('xxhash32');
-        doLoop(done);
-    });
-
-    it('xxHash - 64bit', done => {
-        ClassHelper.changeHashAlgorithm('xxhash64');
-        doLoop(done);
-    });
+    it('MD5', done => doLoop('md5', done));
+    it('xxHash - 32bit', done => doLoop('xxhash32', done));
+    it('xxHash - 64bit', done => doLoop('xxhash64', done));
 
 });
